c9c78cbaa52efbe58a51ff27930f43e3e6bee89a
Alex L. Deweert <alexander.deweert@gmail.com>
2024-02-04 12:35:18 -0700

    added primevue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,8 +1,10 @@
 <template>
     <div>{{ name }} works!!!</div>
+    <Button>Click me</Button>
 </template>
 
 <script setup lang="ts">
+import Button from 'primevue/button'
 let name = 'Poop'
 let data: Array<string> = [
     'Monday',
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
 import { createPinia } from 'pinia'
+import 'primevue/resources/themes/aura-light-green/theme.css'
 
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(PrimeVue)
 app.use(pinia)
 app.use(router)
 
