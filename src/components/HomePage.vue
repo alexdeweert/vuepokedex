b5d46355bf6529c54a4a0605b79735a1b7e26ed7
Alex L. Deweert <alexander.deweert@gmail.com>
2024-02-10 13:37:38 -0700

    updated readme

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -6,15 +6,6 @@
 <script setup lang="ts">
 import Button from 'primevue/button'
 let name = 'Poop'
-let data: Array<string> = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
-]
 </script>
 
 <style scoped></style>
--- src/components/PokemonDetails.vue
+++ src/components/PokemonDetails.vue
@@ -1,15 +1,17 @@
 <template>
     <div v-if="pokemonId">PokemonDetails Works! {{ pokemonId }} - {{ pokemonStore.count }}</div>
-    <button @click="pokemonStore.increment">Click me</button>
+    <Button @click="pokemonStore.increment">Click me</Button>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import { usePokemonStore } from '@/stores/pokemon'
+import Button from 'primevue/button'
 const route = useRoute()
 const pokemonId = computed(() => route.params.pokemonId)
 const pokemonStore = usePokemonStore()
 </script>
 
-<style></style>
+<style>
+</style>
